f1046e3c23cbb168a2e6559437fb661744765fd6
Caio Cesar de Morais Sales <caio.cesar@catijr.com.br>
2020-06-23 01:34:08 -0300

    Rota listagem de Devs backend

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,7 +1,15 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
+const { index } = require('../models/utils/PointSchema');
+
+/* Maximo de cinco funções: Index, show, store, update and destroy */
 
 module.exports = {
+    async index(request, response){
+        const devs = await Dev.find();
+        return response.json(devs);
+    },
+
     async store (request, response){
         const { github_username, techs, latitude, longitude } = request.body;
 
