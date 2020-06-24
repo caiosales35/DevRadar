0b01b1686bacbaa38c90f464b1718833818f39bf
Caio Cesar de Morais Sales <caio.cesar@catijr.com.br>
2020-06-24 15:33:54 -0300

    Form de cadastro frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import './global.css';
+import './App.css';
+import './Sidebar.css'
 
 // Componenete: função que retorna algum conteudo HTML/CSS/JS (para interfaces). O mais isolado possivel, 
 //                um componenete por arquivo. Envolver com <div></div> ou fragmet <></>.
@@ -7,7 +10,35 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>Ola Mundo!</h1>
+      <div id="app">
+          <aside>
+              <strong>Cadastrar</strong>
+              <form>
+                  <div className="input-block">
+                    <label htmlFor="github_username">Usuario do Github</label>
+                    <input name="github_username" id="github_username" required />
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="techs">Tecnologias</label>
+                    <input name="techs" id="techs" required />
+                  </div>
+                  <div className="input-group">
+                    <div className="input-block">
+                      <label htmlFor="latitude">Latitude</label>
+                      <input name="latitude" id="latitude" required />
+                    </div>
+                    <div className="input-block">
+                      <label htmlFor="longitude">Longitude</label>
+                      <input name="longitude" id="longitude" required />
+                    </div>
+                  </div>
+                  <button type="submit">Salvar!</button>
+              </form>
+          </aside>
+          <main>
+
+          </main>
+      </div>
   );
 }
 
