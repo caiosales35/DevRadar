c2269e2163905ce09a6c7a6ba95215a7916d481b
Caio Cesar de Morais Sales <caio.cesar@catijr.com.br>
2020-07-02 22:16:21 -0300

    Botao de busca no mapa mobile

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Image, View, Text } from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
+import { MaterialIcons } from "@expo/vector-icons";
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync,
@@ -33,28 +41,44 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{}}>
-        <Image
-          style={styles.avatar}
-          source={{
-            uri:
-              "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-          }}
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{}}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri:
+                "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+            }}
+          />
+          <Callout
+            onPress={() => {
+              navigation.navigate("Profile", {
+                github_username: "caiosales35",
+              });
+            }}
+          >
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Caio Sales</Text>
+              <Text style={styles.devBio}>Bio</Text>
+              <Text style={styles.devTechs}>Tecnologias</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
         />
-        <Callout
-          onPress={() => {
-            navigation.navigate("Profile", { github_username: "caiosales35" });
-          }}
-        >
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Caio Sales</Text>
-            <Text style={styles.devBio}>Bio</Text>
-            <Text style={styles.devTechs}>Tecnologias</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -84,5 +108,38 @@ const styles = StyleSheet.create({
   },
   devTechs: {
     marginTop: 5,
+  },
+  searchForm: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: "row",
+  },
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#FFF",
+    color: "#333",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 2,
+  },
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#8E4Dff",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 15,
   },
 });
